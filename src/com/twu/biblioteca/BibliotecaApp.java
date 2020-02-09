1e9c6f9d82ad267f5b104621ba22abf8cb20f93b
Larissa Guenther <larissaguenther@Larissas-MacBook-Pro.local>
2020-02-09 09:37:55 +0100

    (Story 1.1) Display welcome message refactored

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,9 +1,18 @@
 package com.twu.biblioteca;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 public class BibliotecaApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, world!");
+        PrintStream outputStream =  new PrintStream(new FileOutputStream(FileDescriptor.out));
+
+
+        CommandLineInterface commandLineInterface = new CommandLineInterface(outputStream);
+        commandLineInterface.displayWelcomeMessage();
     }
 }
